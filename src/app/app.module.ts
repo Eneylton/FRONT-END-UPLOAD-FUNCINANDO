6fb7e0542e3aa559fb876456ead92a07b3c7745d
eneylton <eneylton@hotmail.com>
2019-06-04 20:32:39 -0300

    IONIC-UPLOAD - SEM BANCO

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { HttpClientModule } from '@angular/common/http';
+
 import { MyApp } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
@@ -21,6 +23,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'page-home',
@@ -7,7 +8,21 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,private http: HttpClient) {
+
+  }
+
+  inputFilaChange(event){
+
+  if (event.target.files && event.target.files[0]){
+          const foto = event.target.files[0];
+          const formData = new FormData();
+          formData.append('foto', foto);
+
+          this.http.post('http://localhost:8082/fotos', formData)
+          .subscribe(reposta=> console.log('Upload ok.'));
+    
+  }
 
   }
 
